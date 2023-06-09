500c9f0af0513d58ca7c7f2039bf96181c0e0c19
Yury Dudin <cepob33@gmail.com>
2023-06-09 22:17:16 +0500

    ex41 complite

---
+++ ex41/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(int[] array, int M)
+{
+    for (int i = 0; i < M; i++)
+    {
+        Console.WriteLine("Введите элемент массива под номером: " + (i+1));
+        array[i] = int.Parse(Console.ReadLine());
+    }
+}
+
+void PositiveNumCounter(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count += 1;
+        } 
+    }
+    Console.WriteLine("Колличество элементов массива, которые больше 0: " + count);
+
+}
+
+Console.WriteLine("Введите колличество элементов массива");
+int N = int.Parse(Console.ReadLine());
+int[] massiv = new int[N];
+FillArray(massiv, N);
+PositiveNumCounter(massiv);
